fbafff33a3ec03ee813dd08c1ba6e13792476d04
ylexus <11aPaLEX>
2020-01-26 18:08:37 +0000

    add URL bar to the login dialog

--- src/main/java/net/yudichev/googlephotosupload/ui/LoginDialogFxControllerImpl.java
+++ src/main/java/net/yudichev/googlephotosupload/ui/LoginDialogFxControllerImpl.java
@@ -1,9 +1,15 @@
 package net.yudichev.googlephotosupload.ui;
 
+import javafx.scene.control.TextField;
 import javafx.scene.web.WebView;
 
 public final class LoginDialogFxControllerImpl implements LoginDialogFxController {
     public WebView webView;
+    public TextField urlTextField;
+
+    public void initialize() {
+        webView.getEngine().locationProperty().addListener((observable, oldValue, newValue) -> urlTextField.setText(newValue));
+    }
 
     @Override
     public void load(String url) {
