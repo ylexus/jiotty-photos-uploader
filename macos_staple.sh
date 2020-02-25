ebad9d885083ad80f48079f5ae4157a9e9e7517f
ylexus <11aPaLEX>
2020-02-25 00:06:28 +0000

    do not hard code dmg file name

--- macos_staple.sh
+++ macos_staple.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 PROJECT_DIR="$PWD"
 
-xcrun stapler staple "${PROJECT_DIR}/build/jpackage/Jiotty Photos Uploader-1.0.1.dmg"
+xcrun stapler staple ${PROJECT_DIR}/build/jpackage/*.dmg
