9ec56f0c5424fb9ce4404e606497d155ef8d0322
ylexus <alexey@yudichev.net>
2020-07-29 01:06:57 +0100

    add more logging around resource locale selection

--- src/main/java/net/yudichev/googlephotosupload/core/ResourceBundleModule.java
+++ src/main/java/net/yudichev/googlephotosupload/core/ResourceBundleModule.java
@@ -2,14 +2,26 @@ package net.yudichev.googlephotosupload.core;
 
 import net.yudichev.jiotty.common.inject.BaseLifecycleComponentModule;
 import net.yudichev.jiotty.common.inject.ExposedKeyModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public final class ResourceBundleModule extends BaseLifecycleComponentModule implements ExposedKeyModule<ResourceBundle> {
+    private static final Logger logger = LoggerFactory.getLogger(ResourceBundleModule.class);
+
     public static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle("i18n.Resources", new ResourceBundle.Control() {
+        @Override
+        public List<Locale> getCandidateLocales(String baseName, Locale locale) {
+            var candidateLocales = super.getCandidateLocales(baseName, locale);
+            logger.info("Candidate locales: {}", candidateLocales);
+            return candidateLocales;
+        }
+
         @Override
         public Locale getFallbackLocale(String baseName, Locale locale) {
             // coarse support for CHS/CHT
@@ -25,6 +37,10 @@ public final class ResourceBundleModule extends BaseLifecycleComponentModule imp
             return super.getFallbackLocale(baseName, locale);
         }
     });
+
+    static {
+        logger.info("Using resource bundle locale {}", RESOURCE_BUNDLE.getLocale());
+    }
 
     @Override
     protected void configure() {
