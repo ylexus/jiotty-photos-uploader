376942b0a38b231b151877f5a75ba1e9d47f8e06
ylexus <alexey@yudichev.net>
2020-09-27 17:02:35 +0100

    hyperbuild support: invoke Windows build VM as part of build

---
+++ pull_and_build.bat
@@ -0,0 +1,16 @@
+@echo off
+cd /D "%~dp0"
+if [%1]==[] goto error
+if not exist build mkdir build
+set LOG="pull_and_build.log"
+del %LOG%
+git pull >>%LOG% 2>&1
+echo Version=%1 >>%LOG%
+gradlew clean fullPackage -DVERSION=%1 >>%LOG% 2>&1
+goto eof
+
+:error
+echo "Missing version argument"
+exit /b 1
+
+:eof
