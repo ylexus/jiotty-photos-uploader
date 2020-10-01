c4abf12121606904c3051b42f1cdcdb1ebb34eb4
ylexus <alexey@yudichev.net>
2020-10-01 11:36:37 +0100

    hyperbuild support: iteration 6

--- pull_and_build.sh
+++ pull_and_build.sh
@@ -10,8 +10,10 @@ cd "$(dirname "${0}")" || exit 2
 mkdir -p build
 LOG="pull_and_build.log"
 rm "${LOG}"
+export JAVA_HOME="$HOME/java/jdk-14"
 {
-  git fetch --all ; git reset --hard origin/master;
-  echo "Version=${VERSION}";
-  ./gradlew clean fullPackage "-DVERSION=${VERSION}" "-DCLIENT_SECRET_PATH=$HOME/java/clientSecret.json";
+  git fetch --all
+  git reset --hard origin/master
+  echo "Version=${VERSION}"
+  ./gradlew clean fullPackage "-DVERSION=${VERSION}" "-DCLIENT_SECRET_PATH=$HOME/java/clientSecret.json"
 } >>"${LOG}" 2>&1
