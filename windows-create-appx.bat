dcb7537192a651a187850f3f3942ac0ddffe1c87
ylexus <alexey@yudichev.net>
2021-01-24 21:40:10 +0000

    fix path to newer version of signtool

--- windows-create-appx.bat
+++ windows-create-appx.bat
@@ -7,7 +7,7 @@ echo Running as %USERNAME%
 "%LOCALAPPDATA%\Microsoft\WindowsApps\MsixPackagingTool.exe" create-package --template build\packaging-resources\windows\out\msix-template.xml
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-"C:\Program Files\WindowsApps\Microsoft.MsixPackagingTool_1.2020.709.0_x64__8wekyb3d8bbwe\SDK\signtool.exe" sign /fd SHA256 /sha1 6bbfc8540686d8fc3ae5e394e7b590d903504d39 /tr http://timestamp.comodoca.com /td sha256 build\fullpackage\Windows10-jiotty-photos-uploader-%1.msix
+"C:\Program Files\WindowsApps\Microsoft.MsixPackagingTool_1.2020.1006.0_x64__8wekyb3d8bbwe\SDK\signtool.exe" sign /fd SHA256 /sha1 6bbfc8540686d8fc3ae5e394e7b590d903504d39 /tr http://timestamp.comodoca.com /td sha256 build\fullpackage\Windows10-jiotty-photos-uploader-%1.msix
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 goto :eof
