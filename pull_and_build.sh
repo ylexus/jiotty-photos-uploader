41c54e34ba3dc8f07f91a2392c1cdc9fb6f7cb38
ylexus <alexey@yudichev.net>
2021-06-24 00:44:31 +0100

    #113 iteration 3 of remote RPi build support

--- pull_and_build.sh
+++ pull_and_build.sh
@@ -15,7 +15,7 @@ cd "$(dirname "${0}")" || exit 2
 mkdir -p build
 LOG="pull_and_build.log"
 rm -f "${LOG}"
-export JAVA_HOME="$HOME/java/jdk"
+[[ "$(uname -a)" != *raspberrypi* ]] && export JAVA_HOME="$HOME/java/jdk"
 {
   git fetch --all
   git reset --hard origin/master
