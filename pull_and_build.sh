51506d7fa4f466d88036f7dfe9dfa71d331b1caa
ylexus <alexey@yudichev.net>
2021-06-24 01:14:05 +0100

    #113 iteration 4 of remote RPi build support

--- pull_and_build.sh
+++ pull_and_build.sh
@@ -20,5 +20,10 @@ rm -f "${LOG}"
   git fetch --all
   git reset --hard origin/master
   echo "Version=${VERSION}"
-  ./gradlew clean fullPackage "-DVERSION=${VERSION}" "-DCLIENT_SECRET_PATH=$HOME/java/clientSecret.json"
+  if [[ "$(uname -a)" == *raspberrypi* ]]; then
+    RPI_ARG="-PraspberryPi"
+  else
+    RPI_ARG=""
+  fi
+  ./gradlew clean fullPackage "${RPI_ARG}" "-DVERSION=${VERSION}" "-DCLIENT_SECRET_PATH=$HOME/java/clientSecret.json"
 } >>"${LOG}" 2>&1
